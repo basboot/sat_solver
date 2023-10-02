b1229b669858284ebb8c695e817a8f901891159b
Bas Boot <github@basboot.nl>
2023-10-02 23:03:05 +0200

    Comments

--- river_crossing.py
+++ river_crossing.py
@@ -32,8 +32,10 @@ BOAT_SIZE = 2
 
 
 
+# create dummy variable (which will result to true) to start building clauses
 DUMMY = aiger.atom('dummy')
 
+# create variables for the problem
 variables = {}
 reverse_variables = {}
 n_variables = 0
@@ -45,19 +47,19 @@ for character in CHARACTERS + ['boat']:
 
 if __name__ == '__main__':
     # define start and goal
-    # start with something undefined to combine the conjuction
+    # start with dummy variable to combine the conjuction
     init = DUMMY
     for character in CHARACTERS + ['boat']:
         init = init & ~variables[character + '0']
 
-    # start with something undefined to combine the conjuction
+    # start with dummy variable to combine the conjuction
     goal = DUMMY
     for character in CHARACTERS + ['boat']:
         goal = goal & variables[character + str(TIMESTEPS)]
 
     expr = init & goal
 
-    # define illegal combinations
+    # define dangerous (illegal) combinations
     for t in range(TIMESTEPS + 1):  # 0 to TIMESTEPS (incl)
         for danger in DANGER:
             dangerous_character = danger[0]
@@ -67,23 +69,26 @@ if __name__ == '__main__':
             # start with a contradiction to combine the disjunction
             danger_clause = DUMMY == ~DUMMY
             for vulnerable_character in vulnerable_characters:
+                # any vulnerable_character is together with dangerous_character
                 danger_clause = danger_clause | (variables[dangerous_character + str(t)] == variables[vulnerable_character + str(t)])
-
+            # without protecting_character
             danger_clause = danger_clause & (variables[dangerous_character + str(t)] == ~variables[protecting_character + str(t)])
 
+            # is NOT allowed
             expr = expr & ~danger_clause
 
     # define transitions
     for t in range(1, TIMESTEPS + 1):  # 1 to TIMESTEPS (incl)
-        # boat always crosses
+        # boat always crosses, so it cannot be at the same side the next timestep
         boat_crosses = (variables['boat' + str(t - 1)] == ~variables['boat' + str(t)])
-        # at least one boat operator must cross
+
+        # at least one boat operator must cross (cannot be at the same side)
         # start with a contradiction to combine the disjunction
         operator_crossing = DUMMY == ~DUMMY
         for boat_operator in BOAT_OPERATORS:
             operator_crossing = operator_crossing | (variables[boat_operator + str(t - 1)] == ~variables[boat_operator + str(t)])
 
-        # max boat size total crossings
+        # max boat size total crossings (more than BOATS_SIZE + 1 cannot change sides)
         upperbound_combinations = itertools.combinations(CHARACTERS, BOAT_SIZE + 1)
         max_boat_size_crossings = DUMMY
         for combination in upperbound_combinations:
@@ -92,13 +97,14 @@ if __name__ == '__main__':
                 too_large_crossing = too_large_crossing & (variables[character + str(t - 1)] == ~variables[character + str(t)])
             max_boat_size_crossings = max_boat_size_crossings & ~too_large_crossing
 
-        # crossings can only be in the direction of the boat
+        # crossings can only be in the direction of the boat (change side at time t must be at same as the boat variable)
         legal_crossings = DUMMY
         for character in CHARACTERS:
             with_boat = (variables[character + str(t)] == variables['boat' + str(t)])
             crossing = (variables[character + str(t - 1)] == ~variables[character + str(t)])
             legal_crossings = legal_crossings & crossing.implies(with_boat)
 
+        # combine all constraints for this timestep, and add them to the problem
         expr = expr & boat_crosses & operator_crossing & max_boat_size_crossings & legal_crossings
 
     solver.add_expr(expr)
